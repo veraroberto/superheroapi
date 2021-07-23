77ecba9b2047340cabf7fd1d4e20ad0aa172ee90
Roberto Vera <robertovera89@gmail.com>
2021-07-23 01:10:18 -0500

    All files

---
+++ app.py
@@ -0,0 +1,62 @@
+import pymongo
+from flask import Flask, jsonify, render_template
+
+
+# def call_superHeroe():
+#     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+#     mydb = myclient["superheroes_db"]
+#     mycol = mydb["api_superheroes"]
+
+#     mydoc = mycol.find({})
+
+#     jsonHero = []
+#     for x in mydoc:
+#         del x['_id']
+#         jsonHero.append(x["name"])
+
+#     return jsonHero
+
+
+# # 2. Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+
+# 3. Define what to do when a user hits the index route
+@app.route("/")
+def home():
+
+    return render_template("index.html")
+
+
+@app.route("/api")
+def api():
+
+    # Replace <password> with the password for the robertovera_db user.
+    # Replace myFirstDatabase with the name of the database that connections will use by default.
+    # Ensure any option params are URL encoded.
+
+    # client = pymongo.MongoClient("mongodb+srv://robertovera_db:<password>@cluster0.g7ylf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+  password = "Ffu09ucvMbUB147Y"
+  myFirstDatabase = "SuperHeroes"
+  client_url = "mongodb+srv://robertovera_db:" + password + "@cluster0.g7ylf.mongodb.net/" + myFirstDatabase + "?retryWrites=true&w=majority"
+  
+  myclient = pymongo.MongoClient(client_url)
+
+  mydb = myclient.SuperHeroes
+
+  mydoc = mydb["SuperHeroesJson"].find({})
+
+
+
+  jsonHero = []
+  for x in mydoc:
+    del x['_id']
+    jsonHero.append(x)
+  
+  
+  return jsonify(jsonHero)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/indexapp.js
@@ -0,0 +1,107 @@
+
+var SubjectID = d3.select("#selDataset");
+var SuperHeroInfo = d3.select("#sample-metadata");
+var SuperHeroImage = d3.select("#hero-image")
+
+var importJSON = d3.json("/api");
+
+// Creates the Dropdown Menu
+importJSON.then((incomingData) => {
+  SubjectID.append('option').text("--ID--")
+  incomingData.map(Hero => {
+    SubjectID.append("option").text(Hero['id']);
+  })
+});
+
+
+
+
+
+function optionChanged() {
+  var noID = parseInt(SubjectID.property('value'));
+  createGraphs(noID);
+
+};
+
+
+
+
+function createGraphs(noID) {
+
+  importJSON.then((incomingData) => {
+
+    var HeroIDInfo = incomingData.find((ID) => parseInt(ID['id']) === parseInt(noID));
+    var nameHero = HeroIDInfo['name'];
+    var HeroID = HeroIDInfo['id'];
+    var biography = HeroIDInfo['biography'];
+
+    createSuperHeroInfo(nameHero, HeroID, biography);
+
+    imgURL = HeroIDInfo['image']['url'];
+    createHeroImage(imgURL);
+
+  });
+};
+
+
+function createSuperHeroInfo(name, id, biography) {
+  SuperHeroInfo.html("");
+  SuperHeroInfo.append("li")
+    .text(`name: ${name}`);
+
+  SuperHeroInfo.append("li")
+    .text(`id: ${id}`);
+
+  for (const [key, value] of Object.entries(biography)) {
+    // console.log(`${key}: ${value}`);
+    SuperHeroInfo.append("li")
+      .text(`${key}: ${value}`)
+  }
+};
+
+function createHeroImage(url) {
+
+  var svg = d3.select("#hero-image")
+    .html("")
+    .append("img")
+    .attr("height", "600")
+    .attr("width", "400")
+    .classed("center", true)
+
+  try {
+   
+    svg.attr("src", url);
+    console.log("Error")
+  }
+  catch {
+    // console.error(error);
+    errorURL = "https://zui.zywave.com/images/communications/messages/401-error-message.svg";
+    console.log(errorURL)
+    // svg.attr("src", errorURL);
+  }
+
+}
+
+
+
+function getRandomInt() {
+  return Math.floor(Math.random() * 731);
+};
+
+
+// The First Super Hero is Randomnly Generated
+createGraphs(getRandomInt());
+
+
+function createRandomHero() {
+  randomNum = getRandomInt();
+  createGraphs(randomNum);
+};
+
+
+function searchbyID() {
+  var inputID = d3.select("#inputID").property("value");
+  createGraphs(inputID);
+}
+
+
